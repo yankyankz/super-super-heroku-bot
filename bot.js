3e3f626fd8f5c26069aff0f2c4fd54a343d6aade
yankyankz <33561474+yankyankz@users.noreply.github.com>
2017-11-11 14:16:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.Mzc4Nzg1Nzk2OTYzOTU4ODA1.DOgjWA.0Vx8sFzGliDbWLz3PGwcjBzrw9g);
